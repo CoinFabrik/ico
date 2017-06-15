60a95922276eac2d182dfaafddea8545232b4357
Sebastián <sebastian.nale@nektra.com>
2017-06-15 19:50:44 -0300

    Added gas price to truffle config.

--- truffle.js
+++ truffle.js
@@ -5,7 +5,8 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       from: "0x4cdabc27b48893058aa1675683af3485e4409eff",
-      gas: 4612388
+      gas: 4612388,
+      gasPrice: 20000000000
     }
   }
 };
