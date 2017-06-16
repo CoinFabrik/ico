592ae3f762d2c78339d387d233927dd7c7f15ce5
VeraBE <verabogdanichespina@gmail.com>
2017-06-16 16:21:51 -0300

    Add finalize agent

--- migrations/3_deploy_ICO_contracts.js
+++ migrations/3_deploy_ICO_contracts.js
@@ -3,6 +3,7 @@ var CrowdsaleToken = artifacts.require("./CrowdsaleToken.sol");
 var FlatPricing = artifacts.require("./FlatPricing.sol");
 var MultiSigWallet = artifacts.require("./MultiSigWallet.sol");
 var MintedEthCappedCrowdsale = artifacts.require("./MintedEthCappedCrowdsale.sol");
+var BonusFinalizeAgent = artifacts.require("./BonusFinalizeAgent.sol");
 
 // TODO: adapt to client needs
 var TOKEN_NAME = "TokenI";
@@ -15,14 +16,21 @@ var START_DATE = 1498867200;
 var END_DATE = 1500000000;
 var MINIMUM_FUNDING_GOAL = 1000;
 var WEI_CAP = 1000000000;
+var BONUS_BASE_POINTS = 300000; // equivalent to 30%
 
 module.exports = function(deployer) {
     deployer.deploy(SafeMathLib);
     deployer.link(SafeMathLib, CrowdsaleToken);
     deployer.link(SafeMathLib, FlatPricing);
     deployer.link(SafeMathLib, MintedEthCappedCrowdsale);
+    deployer.link(SafeMathLib, BonusFinalizeAgent);
 
-    deployer.deploy(CrowdsaleToken, TOKEN_NAME, TOKEN_SYMBOL, INITIAL_SUPPLY, DECIMALS, MINTABLE)
+    deployer.deploy(CrowdsaleToken,
+        TOKEN_NAME,
+        TOKEN_SYMBOL,
+        INITIAL_SUPPLY,
+        DECIMALS,
+        MINTABLE)
     .then(function() {
         deployer.deploy(FlatPricing, PRICE)
         .then(function() {
@@ -36,9 +44,22 @@ module.exports = function(deployer) {
                     START_DATE, 
                     END_DATE, 
                     MINIMUM_FUNDING_GOAL, 
-                    WEI_CAP 
-                    );
-                // TODO: set finalize agent
+                    WEI_CAP)
+                .then(function() {
+                    deployer.deploy(BonusFinalizeAgent,
+                        CrowdsaleToken.address,
+                        MintedEthCappedCrowdsale.address,
+                        BONUS_BASE_POINTS,
+                        MultiSigWallet.address)
+                    .then(function() {
+                        MintedEthCappedCrowdsale.deployed().then(function(instance) {
+                            instance.setFinalizeAgent(BonusFinalizeAgent.address);
+                        });
+                        CrowdsaleToken.deployed().then(function(instance) {
+                            instance.setMintAgent(BonusFinalizeAgent.address, true);
+                        });
+                    });
+                });
             });
         });
     });
