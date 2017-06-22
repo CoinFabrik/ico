4abc83520a8be6fb01fb52459d79c5435b205dee
Sebastián Nale <sebastian.nale@nektra.com>
2017-06-22 15:33:21 -0300

    Added local_dev network to provide config for testrpc.

--- truffle.js
+++ truffle.js
@@ -7,6 +7,13 @@ module.exports = {
       from: "0x4cdabc27b48893058aa1675683af3485e4409eff",
       gas: 4612388,
       gasPrice: 20000000000
+    },
+    local_dev: {
+      host: "localhost",
+      port: 20487,
+      network_id: "*",
+      gas: 4612388,
+      gasPrice: 20000000000
     }
   }
 };
