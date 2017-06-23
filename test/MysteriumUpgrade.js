2a96daadafe8b44eda6e536129e1e3f0f13cbc38
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2017-06-23 15:33:04 -0300

    Split test to easier review

--- test/MysteriumUpgrade.js
+++ test/MysteriumUpgrade.js
@@ -3,104 +3,15 @@ var CrowdsaleToken = artifacts.require("./CrowdsaleToken.sol");
 var MysteriumToken = artifacts.require("./MysteriumToken.sol");
 
 contract('Mysterium Upgrade', function(accounts) {
-  // it("Initial check", function() {
-  //   var crowdsale;
-  //   var token;
-  //   return Promise.all([
-  //     MintedEthCappedCrowdsale.deployed(),
-  //     CrowdsaleToken.deployed()
-  //   ]).then(function([crowdsaleInstance, tokenInstance]) {
-  //     crowdsale = crowdsaleInstance;
-  //     token = tokenInstance;
-  //     return crowdsale.getState.call();
-  //   }).then(function(state) {
-  //     // console.log(state.toNumber());
-  //     assert.equal(state.valueOf(), 3, "Should be in funding");
-  //   });
-  // });
-  // it("Funding Params", function() {
-  //   var crowdsale;
-  //   var token;
-  //   return Promise.all([
-  //     MintedEthCappedCrowdsale.deployed(),
-  //     CrowdsaleToken.deployed()
-  //   ]).then(function([crowdsaleInstance, tokenInstance]) {
-  //     crowdsale = crowdsaleInstance;
-  //     token = tokenInstance;
-  //     return Promise.all([
-  //         crowdsale.getState.call(),
-  //         crowdsale.startsAt.call(),
-  //         crowdsale.endsAt.call(),
-  //         crowdsale.weiCap.call(),
-  //         crowdsale.weiRaised.call(),
-  //     ]);
-  //   }).then(function([state, startsAt, endsAt, weiCap, weiRaised]) {
-  //     //console.log(state.toNumber());
-  //     //console.log(startsAt.toNumber());
-  //     //console.log(endsAt.toNumber());
-  //     //console.log(weiCap.toNumber());
-  //     //console.log(weiRaised.toNumber());
-  //     assert.equal(state.valueOf(), 3, "Should be in funding stage");
-  //     assert.equal(weiCap.valueOf(), 1000, "Should have 1000 in cap");
-  //     assert.equal(weiRaised.valueOf(), 0, "Should have raised 0");
-  //   });
-  // });
-  // it("Funding", function() {
-  //   var crowdsale;
-  //   var token;
-  //   return Promise.all([
-  //     MintedEthCappedCrowdsale.deployed(),
-  //     CrowdsaleToken.deployed()
-  //   ]).then(function([crowdsaleInstance, tokenInstance]) {
-  //     crowdsale = crowdsaleInstance;
-  //     token = tokenInstance;
-  //     return token.balanceOf(accounts[1]);
-  //   }).then(function(balance) {
-  //     assert.equal(balance.valueOf(), 0, "Should not have balance");
-  //     return crowdsale.buy({ from: accounts[1], value: 100 });
-  //   }).then(function() {
-  //     return token.balanceOf(accounts[1]);
-  //   }).then(function(balance) {
-  //     assert.equal(balance.valueOf(), 100, "Should be 100 in funding");
-  //   });
-  // });
-  // it("Funded", function() {
-  //   var crowdsale;
-  //   var token;
-  //   var idxs = [];
-  //   for (var i=0; i<5; ++i) {
-  //     idxs.push(i);
-  //     idxs.push(i);
-  //   }
-  //   return Promise.all([
-  //     MintedEthCappedCrowdsale.deployed(),
-  //     CrowdsaleToken.deployed()
-  //   ]).then(function([crowdsaleInstance, tokenInstance]) {
-  //     crowdsale = crowdsaleInstance;
-  //     token = tokenInstance;
-  //     return Promise.all(idxs.map((i) => crowdsale.buy({ from: accounts[i], value: 100 })));
-  //   }).then(function() {
-  //     return crowdsale.getState.call();
-  //   }).then(function(state) {
-  //     assert.equal(state.valueOf(), 4, "Should be success");
-  //   //  return Promise.all(idxs.map((i) => token.balanceOf(accounts[i])));
-  //   //}).then(function(balances) {
-  //   //  assert.equal(balances[0].valueOf(), 100, "Should be 100 in funding");
-  //     return crowdsale.finalize();
-  //   }).then(function() {
-  //     return crowdsale.finalized.call();
-  //   }).then(function(finalized) {
-  //     assert.equal(finalized, true, "Should be finalized");
-  //   });
-  // });
-  it("Upgrade", function() {
-    var crowdsale;
-    var token;
-    var upgradeToken;
-    var idxs = [];
-    for (var i=1; i<=5; ++i) {
-      idxs.push(i);
-    }
+  var crowdsale;
+  var token;
+  var upgradeToken;
+  var idxs = [];
+  for (var i=1; i<=5; ++i) {
+    idxs.push(i);
+  }
+
+  it("Check contracts deploy", function() {
     return Promise.all([
       MintedEthCappedCrowdsale.deployed(),
       CrowdsaleToken.deployed(),
@@ -109,40 +20,56 @@ contract('Mysterium Upgrade', function(accounts) {
       crowdsale = crowdsaleInstance;
       token = tokenInstance;
       upgradeToken = upgradeTokenInstance;
-      return Promise.all(idxs.map((i) => crowdsale.buy({ from: accounts[i], value: 200 })));
     }).then(function() {
       return crowdsale.getState.call();
     }).then(function(state) {
-      assert.equal(state.valueOf(), 4, "Should be success");
+      assert.equal(state.valueOf(), 3, "Crowdsale should be in funding stage");
+    });
+  });
+
+  it("Fund crowdsale", function() {
+    return Promise.all(idxs.map((i) => crowdsale.buy({ from: accounts[i], value: 200 })))
+    .then(function() {
+      return crowdsale.getState.call();
+    }).then(function(state) {
+      assert.equal(state.valueOf(), 4, "Crowdsale should be in success stage");
       return crowdsale.finalize();
     }).then(function() {
       return Promise.all([
+        crowdsale.finalized.call(),
         token.getUpgradeState.call(),
         token.totalSupply.call(),
-        upgradeAgent.originalSupply.call(),
       ]);
-    }).then(function([upgradeState, totalSupply, originalSupply]) {
-      assert.equal(upgradeState.valueOf(), 2, "Should be waiting for upgrade");
+    }).then(function([finalized, upgradeState, totalSupply]) {
+      assert.equal(finalized, true, "Crowdsale should have finalized");
+      assert.equal(upgradeState.valueOf(), 2, "Token should be waiting for upgrade");
       assert.equal(totalSupply.valueOf(), 1000, "Token supply should be 1000");
-      assert.equal(originalSupply.valueOf(), 1000, "Original token supply should be 1000");
-      return upgradeToken.setOriginaryToken(CrowdsaleToken.address);
-    }).then(function() {
+    });
+  });
+
+  it("Set token upgrade agent", function() {
+    return upgradeToken.setOriginaryToken(CrowdsaleToken.address)
+    .then(function() {
       return token.setUpgradeAgent(MysteriumToken.address);
     }).then(function() {
       return token.getUpgradeState.call();
     }).then(function(upgradeState) {
-      assert.equal(upgradeState.valueOf(), 3, "Should be ready to upgrade");
-      return token.upgrade(200, { from: accounts[1] });
-    }).then(function() {
+      assert.equal(upgradeState.valueOf(), 3, "Token should be ready to upgrade");
+    });
+  });
+
+  it("Upgrade tokens", function() {
+    return token.upgrade(200, { from: accounts[1] })
+    .then(function() {
       return Promise.all([
         token.balanceOf.call(accounts[1]),
         upgradeToken.balanceOf.call(accounts[1]),
         token.getUpgradeState.call()
       ]);
     }).then(function([originalBalance, upgradedBalance, upgradeState]) {
-      assert.equal(originalBalance.valueOf(), 0, "Should have balance of 0");
-      assert.equal(upgradedBalance.valueOf(), 200, "Should have a balance of 200");
-      assert.equal(upgradeState.valueOf(), 4, "Should be upgrading");
+      assert.equal(originalBalance.valueOf(), 0, "Old token should have balance of 0");
+      assert.equal(upgradedBalance.valueOf(), 200, "New token should have a balance of 200");
+      assert.equal(upgradeState.valueOf(), 4, "Token should be upgrading");
     });
   });
 });
