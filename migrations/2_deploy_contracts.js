71f24937c6aaea7337100bbec66fd565acff6f04
VeraBE <verabogdanichespina@gmail.com>
2017-06-30 11:37:02 -0300

    Change money values in config.js to use ether when numbers are big

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -8,17 +8,6 @@ const Crowdsale = artifacts.require('./Crowdsale.sol');
 
 const config = require('../config.js');
 
-const TOKEN_NAME = config.TOKEN_NAME;
-const TOKEN_SYMBOL = config.TOKEN_SYMBOL;
-const INITIAL_SUPPLY = config.INITIAL_SUPPLY;
-const DECIMALS = config.DECIMALS;
-const MINTABLE = config.MINTABLE;
-const PRICE = config.PRICE;
-const START_DATE = config.START_DATE;
-const END_DATE = config.END_DATE;
-const MINIMUM_FUNDING_GOAL = config.MINIMUM_FUNDING_GOAL;
-const BONUS_BASE_POINTS = config.BONUS_BASE_POINTS;
-
 module.exports = function(deployer) {
     deployer.deploy(SafeMath);
     deployer.link(SafeMath, FlatPricing);
@@ -27,18 +16,18 @@ module.exports = function(deployer) {
     deployer.link(SafeMath, FixedCeiling);
     deployer.link(SafeMath, Crowdsale);
 
-    let CT_contract = [ CrowdsaleToken, TOKEN_NAME, TOKEN_SYMBOL, INITIAL_SUPPLY, DECIMALS, MINTABLE ];
-    let FP_contract = [ FlatPricing, PRICE ];
+    let CT_contract = [ CrowdsaleToken, config.TOKEN_NAME, config.TOKEN_SYMBOL, web3.toWei(config.INITIAL_SUPPLY), config.DECIMALS, config.MINTABLE ];
+    let FP_contract = [ FlatPricing, config.PRICE ];
     let FC_contract = [ FixedCeiling, web3.toWei(config.CHUNKED_MULTIPLE_CAP), web3.toWei(config.LIMIT_PER_ADDRESS) ];
     // TODO: change to use client's MultiSigWallet
     let MW_contract = [ MultiSigWallet, [0x4cdabc27b48893058aa1675683af3485e4409eff], 1 ];
     
     return deployer.deploy([ CT_contract, FP_contract, FC_contract, MW_contract ])
     .then(async function() {
-        await deployer.deploy(Crowdsale, CrowdsaleToken.address, FlatPricing.address, FixedCeiling.address, MultiSigWallet.address, START_DATE,  END_DATE,  MINIMUM_FUNDING_GOAL);
+        await deployer.deploy(Crowdsale, CrowdsaleToken.address, FlatPricing.address, FixedCeiling.address, MultiSigWallet.address, config.START_DATE,  config.END_DATE,  web3.toWei(config.MINIMUM_FUNDING_GOAL));
     })
     .then(async function() {
-        await deployer.deploy(BonusFinalizeAgent, CrowdsaleToken.address, Crowdsale.address, BONUS_BASE_POINTS, MultiSigWallet.address);
+        await deployer.deploy(BonusFinalizeAgent, CrowdsaleToken.address, Crowdsale.address, config.BONUS_BASE_POINTS, MultiSigWallet.address);
     })
     .then(async function() {
         let CT_prom = CrowdsaleToken.deployed()
