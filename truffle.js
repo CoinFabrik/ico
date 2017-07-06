943915fe56c55c61acaed85fa67f2c36f2ecae29
Horacio <horacio.gomez@coinfabrik.com>
2017-07-06 13:12:01 -0300

    changed configuration parameters

--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     ropsten: {
-      host: "192.168.0.149",
+      host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
       from: "0x4cdabc27b48893058aa1675683af3485e4409eff",
