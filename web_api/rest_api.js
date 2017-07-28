7a712da3830cfee7c09b10ee373906f135b65140
Sebastián Nale <sebastian.nale@nektra.com>
2017-07-28 03:28:36 -0300

    Complete implementation of web service and sample minipage.

--- web_api/rest_api.js
+++ web_api/rest_api.js
@@ -1,6 +1,13 @@
 const abi = require("./contracts_abi.js");
 const config = require("./config.js");
 const Web3 = require("web3");
+const express = require("express");
+const app = express();
+const body_parser = require("body-parser");
+const MongoDB = require("mongodb");
+const MongoClient = MongoDB.MongoClient;
+const ObjectID = MongoDB.ObjectID;
+const Url = require("url");
 
 const web3 = new Web3(new Web3.providers.HttpProvider(config.nodeIpPort));
 
@@ -9,6 +16,33 @@ const CS_contract = web3.eth.contract(abi.Crowdsale);
 const crowdsale = CS_contract.at(config.crowdsale.address);
 const ceiling_contract = web3.eth.contract(abi.FixedCeiling);
 
+app.use(body_parser.json());
+app.set("trust proxy", true);
+
+// Defined to avoid waiting on database or blockchain node to respond
+app.head("/", function (request, response) {
+    response.status(200);
+    response.type("json");
+});
+
+app.get("/", function (request, response) {
+    if (request.body.method != "query_crowdsale")
+    get_crowdsale_state().then(function(state) {
+        response.status(200);
+        response.json(state);
+    });
+});
+
+app.post("/", function (request, response) {
+    if (request.body.method != "generate_customer_id")
+    request.body.ip_address = request.ip;
+    generate_customer_id(request.body).then(function(feedback) {
+        response.status(200);
+        response.json(feedback);
+    });
+});
+
+app.listen(8080);
 
 const callbackGenerator = function (resolve, reject) {
     return function (error, value) {
@@ -31,14 +65,14 @@ async function init_async(object, prop_key, promise) {
     object[prop_key] = await promise;
 }
 
-// Eventually we may want to bind calls to a certain block number. (TODO 1)
+// Eventually we may want to bind calls to a certain block number. (TODO)
 async function get_crowdsale_state(block) {
     // We return the amount raised, number of investors,
     // progress of the current phase and current phase number.
-    let state = {};
-    let promises = [];
+    const state = {};
+    const promises = [];
 
-    //TODO 1: bind calls for block number _block_
+    //TODO: bind calls for block number _block_
     promises.push(init_async(state, "wei_raised", async_call(crowdsale.weiRaised.call)));
     promises.push(init_async(state, "investor_count", async_call(crowdsale.investorCount.call)));
     promises.push(init_async(state, "crowdsale_finalized", async_call(crowdsale.finalized.call)));
@@ -58,12 +92,27 @@ async function get_crowdsale_state(block) {
 
 
 //TODO: replace volatile number with database connection
-let customer_id = 0;
-function generate_customer_id(registration_info) {
-    let feedback = {};
-    customer_id++;
-    feedback.customer_id = customer_id;
-    feedback.delegate_call_data = crowdsale.buyWithCustomerId.getData(customer_id);
+// let customer_id = 0;
+let db_connection;
+const db_url = "mongodb://localhost:27017/ico_db";
+
+async function connect_db(url, options) {
+    if (url == undefined)
+        url = db_url;
+    if (options == undefined)
+        options = {};
+    db_connection = await MongoClient.connect(url, options);
+    return db_connection;
+}
+
+async function generate_customer_id(registration_info) {
+    if (db_connection == undefined)
+        await connect_db();
+    const feedback = { customer_id: new ObjectID() };
+    const customer_data = { ip_address: registration_info.ip_address, name: registration_info.name,
+                            email: registration_info.email, customer_id: feedback.customer_id };
+    db_connection.collections("customers").insertOne(customer_data);
+    feedback.delegate_call_data = crowdsale.buyWithCustomerId.getData(feedback.customer_id);
     return feedback;
 }
 
---
+++ web_test/minipage.js
@@ -0,0 +1,38 @@
+
+
+const progress_bar = document.getElementById("progress_bar");
+const submit_button = document.getElementById("submit_button");
+const refresh_button = document.getElementById("refresh_button");
+const name_field = document.getElementById("name_field");
+const email_field = document.getElementById("email_field");
+const id_field = document.getElementById("id_field");
+const call_data_field = document.getElementById("call_data_field");
+const phase_field = document.getElementById("phase_field");
+
+function send_request(method, body, on_ready_state) {
+    const http_req = new XMLHttpRequest();
+    http_req.open(method, "http://192.168.0.167:8080/");
+    http_req.setRequestHeader("Content-Type", "application/json");
+    http_req.onreadystatechange = function() {
+        if (http_req.readyState == XMLHttpRequest.DONE && http_req.status == 200) {
+            on_ready_state(http_req.response);
+        }
+    }
+    http_req.send(JSON.stringify(body));
+}
+
+submit_button.addEventListener("click", () => send_request("POST", { "name": name_field.textContent, "email": email_field.textContent, "method": "generate_customer_id" }, function(response) {
+    id_field.textContent = response.customer_id
+    call_data_field.textContent = response.call_data;
+}));
+
+
+function refresh_state() {
+    send_request("GET", { "method": "query_crowdsale" },  function(response) {
+        let percent = (response.phase_progress / response.chunked_wei_multiple) * 100;
+        progress_bar.style = "width: " + percent.toString() + "%";
+        phase_field.textContent = "Current phase: " + response.current_phase.toString();
+    });
+}
+
+refresh_button.addEventListener("click", () => refresh_state());
