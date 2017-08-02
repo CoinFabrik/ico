9c0c16e7189b707de45b4bb70d061f55e53cfc43
Horacio <horacio.gomez@coinfabrik.com>
2017-08-02 14:14:10 -0300

    lowered crowdsale cap

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 var config = {};
 
-config.tokenName = 'Hubii';
-config.tokenSymbol = 'HI';
+config.tokenName = 'BurgerKoenig';
+config.tokenSymbol = 'BK';
 config.initialSupply = 0; // in ether
 config.decimals = 8;
 config.mintable = true;
 config.price = 0.001; // in ethert
-//SET TIME CORRECLTY FOR GOD SAKE	
+//SET TIME CORRECLTY	
 config.startDate = Date.now()/1000 | 0; //1500645594 + 180 //
-config.endDate = 1501772400; // bla
+config.endDate = config.startDate + 7200; // bla
 config.minimumFundingGoal = 5; // in ether (CHANGE to USD 4m)
 config.bonusBasePoints = 3000; // equivalent to 30%
-config.chunkedMultipleCap = 40; // in ether (CHANGE to USD 5m)
-config.limitPerAddress = 7; // for testing; in ether
+config.chunkedMultipleCap = 25; // in ether (CHANGE to USD 5m)
+config.limitPerAddress = 6; // for testing; in ether
 
 module.exports = config;
