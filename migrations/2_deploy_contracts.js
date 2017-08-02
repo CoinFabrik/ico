04be57015ec71faf4523633c9dcbb72d6ced736a
Horacio <horacio.gomez@coinfabrik.com>
2017-08-02 14:15:09 -0300

    hardcoded an already deployed wallet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -20,14 +20,14 @@ module.exports = function(deployer) {
     let FP_contract = [ FlatPricing, web3.toWei(config.price) ];
     let FC_contract = [ FixedCeiling, web3.toWei(config.chunkedMultipleCap), web3.toWei(config.limitPerAddress) ];
     // TODO: change to use client's MultiSigWallet
-    let MW_contract = [ MultiSigWallet, [0x4cdabc27b48893058aa1675683af3485e4409eff], 1 ];
-    
-    return deployer.deploy([ CT_contract, FP_contract, FC_contract, MW_contract ])
+    // let MW_contract = [ MultiSigWallet, [0x485de458fbcac6a7d35227842d652641384cb333], 1 ];
+    let MW_address = "0x931F6E5c89dD5559D3820cFBd1975BA5d92F87E9";
+    return deployer.deploy([ CT_contract, FP_contract, FC_contract])
     .then(async function() {
-        await deployer.deploy(Crowdsale, CrowdsaleToken.address, FlatPricing.address, FixedCeiling.address, MultiSigWallet.address, config.startDate,  config.endDate,  web3.toWei(config.minimumFundingGoal));
+        await deployer.deploy(Crowdsale, CrowdsaleToken.address, FlatPricing.address, FixedCeiling.address, MW_address, config.startDate,  config.endDate,  web3.toWei(config.minimumFundingGoal));
     })
     .then(async function() {
-        await deployer.deploy(BonusFinalizeAgent, CrowdsaleToken.address, Crowdsale.address, config.bonusBasePoints, MultiSigWallet.address);
+        await deployer.deploy(BonusFinalizeAgent, CrowdsaleToken.address, Crowdsale.address, config.bonusBasePoints, MW_address);
     })
     .then(async function() {
         let CT_prom = CrowdsaleToken.deployed()
@@ -39,7 +39,7 @@ module.exports = function(deployer) {
             console.log('Setting BonusFinalizeAgent as release agent of CrowdsaleToken...');
             await tokenInstance.setReleaseAgent(BonusFinalizeAgent.address);
             // console.log('Transfering ownership of CrowdsaleToken to MultiSigWallet...');
-            // tokenInstance.transferOwnership(MultiSigWallet.address);
+            // tokenInstance.transferOwnership(MW_address);
         });
         
         let C_prom = Crowdsale.deployed()
@@ -47,7 +47,7 @@ module.exports = function(deployer) {
             console.log('Setting BonusFinalizeAgent as finalize agent of Crowdsale...');
             await crowdsaleInstance.setFinalizeAgent(BonusFinalizeAgent.address);
             // console.log('Transfering ownership of Crowdsale contract to MultiSigWallet...');
-            // crowdsaleInstance.transferOwnership(MultiSigWallet.address);
+            // crowdsaleInstance.transferOwnership(MW_address);
         });
 
         await Promise.all([ CT_prom, C_prom ]);
