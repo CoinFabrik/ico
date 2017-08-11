0815264eaa9ff0f43177808b000cc7a5228b3687
Horacio <horacio.gomez@coinfabrik.com>
2017-08-11 17:03:15 -0300

    set blocknumbers for testing

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 var config = {};
 config.tests = {};
 
-
+const Web3 = require("web3");
+const web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.0.186:7999"));
 
 config.tokenName = 'Hubiits';
 config.tokenSymbol = 'HB'; // TODO: check if this is the correct symbol
@@ -10,5 +11,8 @@ config.tokenSymbol = 'HB'; // TODO: check if this is the correct symbol
 config.startBlock = 4137656 + 28239
 // We give two week's worth of blocks for the crowdsale to run its course: 55,486
 config.endBlock = config.startBlock + 55486
+
+config.tests.startBlock = web3.eth.getBlock("latest").number + 10;
+config.tests.endBlock = config.tests.startBlock + 500;
 
 module.exports = config;
