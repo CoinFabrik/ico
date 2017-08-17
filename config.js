f56b823ad7738e90120dbcc88a3770070eedb1ff
Horacio <horacio.gomez@coinfabrik.com>
2017-08-17 12:53:56 -0300

    modified config param

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const Web3 = require("web3");
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 
 //Neededto initialize the crowdsale
-config.MW_address = "0x931F6E5c89dD5559D3820cFBd1975BA5d92F87E9";
+config.MW_address = "0x931F6E5c89dD5559D3820cFBd1975BA5d92F8777";
 config.startBlock = web3.eth.getBlock("latest").number + 50;
 config.endBlock = config.startBlock + 1000;
 
