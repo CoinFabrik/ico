59978b71ee6ae78668232b57bb33a5aa9666f19b
Sebastián Nale <sebastian.nale@nektra.com>
2017-08-25 18:16:37 -0300

    Several simplifications and fixes in HoldableToken.

--- contracts/HoldableToken.sol
+++ contracts/HoldableToken.sol
@@ -9,105 +9,115 @@ import './ERC20Basic.sol';
 import './SafeMath.sol';
 
 /**
- * @title Basic token
- * @dev Basic version of StandardToken, with no allowances. 
+ * @title Holdable Token
+ * @dev Implementation of the simplified interface in ERC20Basic that provides an incentive to hold tokens for a time after the crowdsale ends. 
  */
-contract HoldableToken is ERC20Basic, FractionalERC20, ReleasableToken, MintableToken {
+contract HoldableToken is ERC20Basic {
   using SafeMath for uint;
 
+  uint private constant payments = 14; 
 
-  uint256 revenuePerWeek;
-
-  uint256 blocksBetweenPayments;
-
-  uint256 endBlock;
-
-  uint256 lastCheckedWeek;
-
-  uint256[14] heldTokensPerWeek;
-
-  Crowdsale crowdsale
+  uint blocksBetweenPayments;
+  uint endBlock;
+  uint[] heldTokensPerPayday;
+  Crowdsale crowdsale;
 
   struct Contributor {
-    uint256 primaryTokensBalance;
-    uint256 secondaryTokensBalance;
-    uint256 nextPaymentWeek;
+    uint primaryTokensBalance;
+    uint secondaryTokensBalance;
+    uint nextPayday;
   }
 
   mapping(address => Contributor) public contributors;
 
+  //TODO: add configuration parameters like crowdsale
+  function HoldableToken() internal {
+    heldTokensPerPayday.push(0);
+  }
+
 
   /**
   * @dev transfer token for a specified address
-  * @param _to The address to transfer to.
-  * @param _value The amount to be transferred.
+  * @param destination The address to transfer to.
+  * @param value The amount to be transferred.
   */
-  function transfer(address _to, uint _value) public returns (bool success) { //Mergear con el released REQUERIR TODO LO NECESARIO
+  function transfer(address destination, uint value) public returns (bool success) {
 
-    balance = balanceOf(msg.sender);    
-    require(balance >= _value);
+    uint balance = balanceOf(msg.sender);
+    require(balance >= value);
 
-    if(msg.sender == crowdsale) {
-      contributors[crowdsale].secondaryTokensBalance = contributors[crowdsale].secondaryTokensBalance.sub(_value);
-      contributors[_to].primaryTokensBalance = contributors[_to].primaryTokensBalance.add(_value);
-      heldTokensPerWeek[0] = heldTokensPerWeek[0].add(_value);
+    if (msg.sender == crowdsale && (endBlock == 0 || block.number <= endBlock)) {
+      contributors[crowdsale].secondaryTokensBalance = contributors[crowdsale].secondaryTokensBalance.sub(value);
+      contributors[destination].primaryTokensBalance = contributors[destination].primaryTokensBalance.add(value);
+      heldTokensPerPayday[0] = heldTokensPerPayday[0].add(value);
     } else {
       secondaryBalance = contributors[msg.sender].secondaryTokensBalance.add(pendingRevenue(msg.sender));
-      if(secondaryBalance < _value) {
-        spentInitialTokens = _value.sub(secondaryBalance);
+      if (secondaryBalance < value) {
+        spentInitialTokens = value.sub(secondaryBalance);
         contributors[msg.sender].secondaryTokensBalance = 0;
         contributors[msg.sender].primaryTokensBalance = contributors[msg.sender].primaryTokensBalance.sub(spentInitialTokens);
-        actualPaymentWeek = currentPaymentWeek();
-        lastHeldAmount = 0;
-        for (i = 0; i < actualPaymentWeek; i++) {
-          lastHeldAmount = heldTokensPerWeek[i] > 0 ? heldTokensPerWeek : lastHeldAmount;
-          if(heldTokensPerWeek[i] == 0) {
-            heldTokensPerWeek[i] = lastHeldAmount;
-          }
-        }
-        heldTokensPerWeek[actualPaymentWeek] = heldTokensPerWeek[actualPaymentWeek].sub(spentInitialTokens);
+
+        uint curPayday = currentPayday();
+        updatePaydayAmounts(curPayday);
+
+        heldTokensPerPayday[curPayday] = heldTokensPerPayday[curPayday].sub(spentInitialTokens);
       } else {
-        contributors[msg.sender].secondaryTokensBalance = secondaryBalance.sub(_value);
-        contributors[_to].secondaryTokensBalance = contributors[_to].secondaryTokensBalance.add(_value);
+        contributors[msg.sender].secondaryTokensBalance = secondaryBalance.sub(value);
+        contributors[destination].secondaryTokensBalance = contributors[destination].secondaryTokensBalance.add(value);
       }
 
-      contributors[_owner].nextPaymentWeek = currentPaymentWeek.add(1);      
+      contributors[_owner].nextPayday = curPayday.add(1);
     }
-    Transfer(msg.sender, _to, _value);
+    Transfer(msg.sender, destination, value);
     return true;
   }
 
   /**
   * @dev Gets the balance of the specified address.
-  * @param _owner The address to query the the balance of. 
+  * @param account The address to query the the balance of. 
   * @return An uint representing the amount owned by the passed address.
   */
-  function balanceOf(address _owner) public constant returns(uint) {
-    if(!released) { //OR OTHER FLAG
-      return contributors[_owner].primaryTokensBalance;
-    } else {
-      revenue = pendingRevenue(_owner);
-      balance = revenue.add(contributors[_owner].secondaryTokensBalance).add(contributors[_owner].primaryTokensBalance);
-      return balance;
+  function balanceOf(address account) public constant returns(uint) {
+    uint revenue = pendingRevenue(account);
+    balance = revenue.add(contributors[account].secondaryTokensBalance).add(contributors[account].primaryTokensBalance);
+    return balance;
+  }
+
+  function updatePaydayAmounts(uint curPayday) private {
+    if (heldTokensPerPayday.length() > curPayday)
+      return;
+    uint heldTokens = heldTokensPerPayday[heldTokensPerPayday.length().sub(1)];
+    for (uint i = heldTokensPerPayday.length(); i <= curPayday; i++) {
+      heldTokensPerPayday.push(heldTokens);
     }
   }
 
-  function pendingRevenue(address _owner) internal constant returns(uint) {
-    actualPaymentWeek = currentPaymentWeek();
-    revenue = 0;
+  function revenuePerPayday() internal return (uint);
 
-    for (i = contributors[_owner].nextPaymentWeek; i <= actualPaymentWeek; i++) {
-      lastHeldAmount = heldTokensPerWeek[i] > 0 ? heldTokensPerWeek : lastHeldAmount; 
-      tokensPerHeld = revenuePerWeek.div(lastHeldAmount);
-      revenue = contributors[_owner].primaryTokensBalance.mul(tokensPerHeld).add(revenue);
+  function pendingRevenue(address account) internal constant returns(uint) {
+    uint curPayday = currentPayday();
+    uint revenue = 0;
+    uint heldTokens = 0;
+
+    for (uint i = contributors[account].nextPayday; i <= curPayday; i++) {
+      uint index = i-1;
+      heldTokens = index < heldTokensPerPayday.length() ? heldTokensPerPayday[index] : heldTokens;
+      // TODO: add interface for revenuePerPayday so it is calculated by derived contract.
+      // Warning: overflow in the multiplication will block all operations for the account
+      // The magnitude of the revenue per payday should be chosen to guarantee the safety of all operations.
+      // E.g. if the revenue is taken out of an initial pool of 10^23 units, all operations are safe since
+      // 10^23 * 10^23 = 10^46 < 10^77 ~= 2^256
+      uint pay = contributors[account].primaryTokensBalance.mul(revenuePerPayday()).div(heldTokens);
+      revenue = pay.add(revenue);
     }
     return revenue;
   }
 
-  function currentPaymentWeek() internal constant returns (uint) {
-    actualPaymentWeek = block.number.sub(endBlock).div(blocksBetweenPayments);
-    actualPaymentWeek = actualPaymentWeek > 14 ? 14 : actualPaymentWeek;
-    return actualPaymentWeek;
+  function currentPayday() internal constant returns (uint) {
+    if (block.number <= endBlock || endBlock == 0) return 0;
+    uint curPayday = block.number.sub(endBlock).div(blocksBetweenPayments);
+    curPayday = curPayday > payments ? payments : curPayday;
+    return curPayday;
   } 
 
 }
