f689f13e15e3f81edfad137fc5d9a35db8c521c5
Sebastián Nale <sebastian.nale@nektra.com>
2017-09-06 13:59:35 -0300

    Updated config.js with the crowdsale address.

--- web_api/config.js
+++ web_api/config.js
@@ -3,7 +3,7 @@ const config = {};
 // config.nodeIpPort = "http://127.0.0.1:20487";
 config.ipc_file = process.env.HOME + "/.ethereum/geth.ipc";
 config.crowdsale = {
-  address: "0x07bff081b3978d9f69dc5f7232825bbff3b1666c"
+  address: "0xb9aac097f4dadcd6f06761eb470346415ef28d5a"
 };
 config.ceiling_strategy = {
   address: "0xd80df9c12982e1746c4f3dfc75e267d404527ffc"
