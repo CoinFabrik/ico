a16dd50d4c6e7c273e479c0235b9b910466ef690
Sebastián Nale <sebastian.nale@nektra.com>
2017-09-08 13:41:09 -0300

    Removed unnecessary gas value in deployment.

--- migrations/5_set_whitelist.js
+++ migrations/5_set_whitelist.js
@@ -17,6 +17,6 @@ module.exports = function(deployer, network, accounts) {
             // crowdsale.setEarlyParticipantWhitelist("0x72FC254FCE8DD4B0E92A90035AFE90A218E0F206", true);
             crowdsale.setEarlyParticipantWhitelist("0x8ffC991Fc4C4fC53329Ad296C1aFe41470cFFbb3", true);
 
-            crowdsale.setFundingCap(config.fundingCap, {gas:1000000});
+            crowdsale.setFundingCap(config.fundingCap);
         }); 
 };
