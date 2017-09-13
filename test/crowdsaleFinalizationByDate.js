8d266fff3023d3067ecfa023ea39b27697784642
Sebastián Nale <sebastian.nale@nektra.com>
2017-09-13 12:09:19 -0300

    Fixed missing calls and return in config.

--- test/crowdsaleFinalizationByDate.js
+++ test/crowdsaleFinalizationByDate.js
@@ -1,4 +1,4 @@
-const config = require('../config.js');
+const config = require('../config.js')(web3);
 const assertFail = require('./helpers/assertFail');
 
 const SafeMath = artifacts.require('./SafeMath.sol');
