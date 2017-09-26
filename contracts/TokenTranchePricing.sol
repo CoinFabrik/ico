887560149b4c6bf7494e8632faebcc8f4afc4de8
Sebastián Nale <sebastian.nale@nektra.com>
2017-09-26 12:39:31 -0300

    Extended the tranche pricing contract to support block intervals.

--- contracts/TokenTranchePricing.sol
+++ contracts/TokenTranchePricing.sol
@@ -21,69 +21,64 @@ contract TokenTranchePricing is Ownable {
    * Define pricing schedule using tranches.
    */
   struct Tranche {
-      // Amount in weis when this tranche becomes active
+      // Amount in tokens when this tranche becomes inactive
       uint amount;
+      // Block interval [start, end)
+      // Starting block (included in the interval)
+      uint start;
+      // Ending block (excluded from the interval)
+      uint end;
       // How many tokens per wei you will get while this tranche is active
       uint price;
   }
+  // We define offsets and size for the deserialization of ordered tuples in raw arrays
+  uint private constant amount_offset = 0;
+  uint private constant start_offset = 1;
+  uint private constant end_offset = 2;
+  uint private constant price_offset = 3;
+  uint private constant tranche_size = 4;
 
   Tranche[] public tranches;
 
   /// @dev Contruction, creating a list of tranches
-  /// @param init_tranches Raw array of ordered pairs: (start amount, price)
+  /// @param init_tranches Raw array of ordered tuples: (start amount, start block, end block, price)
   function TokenTranchePricing(uint[] init_tranches) public {
     // Need to have tuples, length check
-    require(init_tranches.length % 2 == 0);
+    require(init_tranches.length % tranche_size == 0);
 
-    uint highestAmount = 0;
-
-    for (uint i = 0; i < init_tranches.length / 2; i++) {
+    tranches.length = init_tranches.length / tranche_size;
+    for (uint i = 0; i < init_tranches.length / tranche_size; i++) {
       // No invalid steps
-      require((i == 0) || (init_tranches[i * 2] > highestAmount));
+      uint constant amount = init_tranches[i * tranche_size + amount_offset];
+      uint constant start = init_tranches[i * tranche_size + start_offset];
+      uint constant end = init_tranches[i * tranche_size + end_offset];
+      require(start < end);
+      // Bail out when entering unnecessary tranches
+      // This is preferably checked before deploying contract into any blockchain.
+      require(i == 0 || (end >= tranches[i - 1].end && amount > tranches[i - 1].amount) ||
+              (end > tranches[i - 1].end && amount >= tranches[i - 1].amount));
 
-      tranches[i].amount = init_tranches[i * 2];
-      tranches[i].price = init_tranches[i * 2 + 1];
-
-      highestAmount = tranches[i].amount;
+      tranches[i].amount = amount;
+      tranches[i].price = init_tranches[i * tranche_size + price_offset];
+      tranches[i].start = start;
+      tranches[i].end = end;
     }
 
     // Last tranche price must be zero, terminating the crowdale
     require(tranches[tranches.length.sub(1)].price == 0);
   }
 
-  /// @dev Iterate through tranches. You reach end of tranches when price = 0
-  /// @return tuple (time, price)
-  function getTranche(uint n) public constant returns (uint, uint) {
-    return (tranches[n].amount, tranches[n].price);
-  }
-
-  function getFirstTranche() private constant returns (Tranche) {
-    return tranches[0];
-  }
-
-  function getLastTranche() private constant returns (Tranche) {
-    return tranches[tranches.length - 1];
-  }
-
-  function getPricingStartsAt() public constant returns (uint) {
-    return getFirstTranche().amount;
-  }
-
-  function getPricingEndsAt() public constant returns (uint) {
-    return getLastTranche().amount;
-  }
-
   /// @dev Get the current tranche or bail out if we are not in the tranche periods.
   /// @param tokensSold total amount of tokens sold, for calculating the current tranche
   /// @return {[type]} [description]
   function getCurrentTranche(uint tokensSold) private constant returns (Tranche) {
-    // TODO: If using an absurd amount of tranches, implement binary search.
-    // Index i is intended to overflow to end the loop
-    for (uint i = tranches.length - 1; i < tranches.length; i--) {
-      if (tokensSold >= tranches[i].amount) {
+    for (uint i = 0; i < tranches.length; i++) {
+      if (tranches[i].start <= block.number && block.number < tranches[i].end &&  tokensSold < tranches[i].amount) {
         return tranches[i];
       }
     }
+    // No tranche is currently active
+    revert();
   }
 
   /// @dev Get the current price.
