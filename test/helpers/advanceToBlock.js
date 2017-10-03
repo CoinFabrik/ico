a522869a2dedb8f6473ad50eec0e1ee399332b67
Horacio <horacio.gomez@coinfabrik.com>
2017-10-03 16:52:05 -0300

    added functions for block mining in testrpc

---
+++ test/helpers/advanceToBlock.js
@@ -0,0 +1,7 @@
+const advanceBlock = require('./advanceBlock');
+
+module.exports = async function advanceToBlock(number) {
+  while (web3.eth.blockNumber < number) {
+    await advanceBlock()
+  }
+}
