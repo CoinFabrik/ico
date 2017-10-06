0949fa406fa16e99c5576fa1b9c9e7773b3ecc7c
Horacio <horacio.gomez@coinfabrik.com>
2017-10-06 16:08:43 -0300

    fixed error

--- test/helpers/durations.js
+++ test/helpers/durations.js
@@ -4,5 +4,5 @@ module.exports = {
   hours:   function(val) { return val * (60*60) },
   days:    function(val) { return val * (24*60*60) },
   weeks:   function(val) { return val * (7*24*60*60) },
-  years:   function(val) { return val * (365*7*24*60*60)} 
+  years:   function(val) { return val * (365*24*60*60)} 
 };
