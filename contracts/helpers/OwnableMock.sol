bd1d668c71a36ea4c2116ba0d0d4947e3a907fe8
Horacio <horacio.gomez@coinfabrik.com>
2017-10-09 15:44:16 -0300

    added Ownable mock contract

---
+++ contracts/helpers/OwnableMock.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.4.11;
+
+
+import '../Ownable.sol';
+
+
+// mock class using BasicToken
+contract OwnableMock is Ownable {
+
+  function OwnableMock() Ownable() public {
+  }
+
+}
