e6deb1bf7e4757b3563a931085a6752eaa111df8
Horacio <horacio.gomez@coinfabrik.com>
2017-11-03 15:42:16 -0300

    updated api testing page

--- web_test/minipage.js
+++ web_test/minipage.js
@@ -1,15 +1,10 @@
-const progress_bar = document.getElementById("progress_bar");
 const submit_button = document.getElementById("submit_button");
 const refresh_button = document.getElementById("refresh_button");
-const phase_field = document.getElementById("phase_field");
-const block_number_field = document.getElementById("block_number_field");
-const starting_block_field = document.getElementById("starting_block_field");
-const ending_block_field = document.getElementById("ending_block_field");
-const start_eta_field = document.getElementById("start_eta_field");
-const end_eta_field = document.getElementById("end_eta_field");
-const funding_cap_field = document.getElementById("funding_cap_field");
-const minimum_goal_field = document.getElementById("minimum_goal_field");
+const starting_timestamp_field = document.getElementById("starting_timestamp_field");
+const ending_timestamp_field = document.getElementById("ending_timestamp_field");
 const investor_count_field = document.getElementById("investor_count_field");
+const minimum_buy_field = document.getElementById("minimum_buy_field");
+const ether_raised_field = document.getElementById("ether_raised_field");
 
 function send_request(method, on_ready_state) {
     const http_req = new XMLHttpRequest();
@@ -37,17 +32,11 @@ function refresh_state() {
         // For debugging purposes
         console.log("Parsed JSON:");
         console.log(res);
-        let percent = (res.phase_progress / res.ether_per_phase) * 100;
-        progress_bar.style = "width: " + percent.toString() + "%";
-        phase_field.textContent = "Current phase: " + res.current_phase.toString();
-        block_number_field.textContent = res.current_block.toString();
-        starting_block_field.textContent = res.starting_block.toString();
-        ending_block_field.textContent = res.ending_block.toString();
-        start_eta_field.textContent = res.start_eta.toString();
-        end_eta_field.textContent = res.end_eta.toString();
-        funding_cap_field.textContent = res.cap.toString();
-        minimum_goal_field.textContent = res.minimum_goal.toString();
+        starting_timestamp_field.textContent = res.starting_timestamp.toString();
+        ending_timestamp_field.textContent = res.ending_timestamp.toString();
         investor_count_field.textContent = res.investor_count.toString();
+        minimum_buy_field.textContent = res.minimum_buy.toString();
+        ether_raised_field.textContent = res.ether_raised.toString();
     });
 }
 
