9dbb0f238d7f915261aba7a6de78728341052d9e
Sebastián Nale <sebastian.nale@nektra.com>
2017-11-17 11:40:43 -0300

    Added two testing addresses to the whitelist.

--- migrations/5_set_whitelist.js
+++ migrations/5_set_whitelist.js
@@ -6,6 +6,9 @@ module.exports = function(deployer, network, accounts) {
   // const config = require('../config.js')(web3, network);
   deployer.then(function() {
     const crowdsale = Crowdsale.at(Crowdsale.address);
+    
+    crowdsale.setEarlyParticipantWhitelist("0x9ddc080f31778dc5c2be20a51483fccd3ce17afd", true);
+    crowdsale.setEarlyParticipantWhitelist("0x25F474bF97eBaFB6D4AC26F2DC897e35cdD088Af", true);
 
     // Testing addresses
     // crowdsale.setEarlyParticipantWhitelist("0x72FC254FCE8DD4B0E92A90035AFE90A218E0F206", true);
