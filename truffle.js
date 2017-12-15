8a18e196643e2906c20a99b03494a804f8427c86
Horacio <horacio.gomez@coinfabrik.com>
2017-12-15 15:41:16 -0300

    changed truffle's default gas for ropsten chain

--- truffle.js
+++ truffle.js
@@ -13,7 +13,7 @@ module.exports = {
       port: 8544,
       network_id: "3", // Match any network id
       // from: "0x485de458fbcac6a7d35227842d652641384cb333", //// Defaults to the first available account provided by your Ethereum client.
-      gas: 4612388,
+      gas: 200000,
       gasPrice: 20000000000
     },
     testrpc: {
