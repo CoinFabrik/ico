d5980df97a593b129b55a61b12be205345b62096
Sebastián Nale <sebastian.nale@nektra.com>
2017-12-26 16:56:27 -0300

    Added investment gateway to GenericCrowdsale.

--- contracts/GenericCrowdsale.sol
+++ contracts/GenericCrowdsale.sol
@@ -29,6 +29,9 @@ contract GenericCrowdsale is Haltable {
   /* ether will be transferred to this address */
   address public multisigWallet;
 
+  /* a contract may be deployed to function as a gateway for investments */
+  address public investmentGateway;
+
   /* the starting time of the crowdsale */
   uint public startsAt;
 
@@ -218,9 +221,15 @@ contract GenericCrowdsale is Haltable {
    *
    * Pay for funding, get invested tokens back in the receiver address.
    */
-  function buyOnBehalf(address receiver) public payable unsignedBuyAllowed {
+  function buyOnBehalf(address receiver) public payable {
+    require(!requiredSignedAddress || msg.sender == investmentGateway)
     require(!requireCustomerId); // Crowdsale needs to track participants for thank you email
     investInternal(receiver, 0);
+  }
+
+  function setInvestmentGateway(address gateway) public onlyOwner {
+    require(gateway != address(0));
+    investmentGateway = gateway;
   }
 
   /**
