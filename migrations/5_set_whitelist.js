d9fb3fb6c094fdd408f7c3592484d9b32f5d77e1
Sebastián Nale <sebastian.nale@nektra.com>
2018-01-04 15:06:25 -0300

    Added signer address to deployment scripts.

--- migrations/5_set_whitelist.js
+++ migrations/5_set_whitelist.js
@@ -7,6 +7,8 @@ module.exports = function(deployer, network, accounts) {
   deployer.then(function() {
     const crowdsale = Crowdsale.at(Crowdsale.address);
 
+    // Configure the signing address for contributions
+    crowdsale.setRequireSignedAddress(true, "0x9773cc14E40853917B9a8fB49de4ABdEa736ED1E");
     // Testing addresses
     // crowdsale.setEarlyParticipantWhitelist("0x72FC254FCE8DD4B0E92A90035AFE90A218E0F206", true);
     // crowdsale.setEarlyParticipantWhitelist("0x8ffC991Fc4C4fC53329Ad296C1aFe41470cFFbb3", true);
