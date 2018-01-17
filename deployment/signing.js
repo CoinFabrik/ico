f9f743279ae3ab2cc3def327df47c03747bf4f6a
Horacio <horacio.gomez@coinfabrik.com>
2018-01-17 14:21:07 -0300

    scripts updated to deployment version

--- deployment/signing.js
+++ deployment/signing.js
@@ -8,6 +8,6 @@ const account = "0x54d9249C776C56520A62faeCB87A00E105E8c9Dc";
 // Setup contract objects
 const CS_contract = web3.eth.contract(cs_abi);
 
-const CS_instance = CS_contract.at("0x4527f9d48b6788cA32B9FED8B6b090B23189214c");
+const CS_instance = CS_contract.at("0xb9aC67A866b2e6990538ad5C9c20b830E5404518");
 
-CS_instance.setRequireSignedAddress(true, "0x9773cc14E40853917B9a8fB49de4ABdEa736ED1E", {from: account, gasPrice: 1000000000, gas:200000});
+CS_instance.setRequireSignedAddress(true, "0x9773cc14E40853917B9a8fB49de4ABdEa736ED1E", {from: account, gasPrice: 3000000000, gas:200000, nonce: 1451});
