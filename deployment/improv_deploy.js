5459692695c163d4aa5c01c82aca440f33db025e
Horacio <horacio.gomez@coinfabrik.com>
2018-01-19 22:24:05 -0300

    changed gas price and rpc port

--- deployment/improv_deploy.js
+++ deployment/improv_deploy.js
@@ -1,5 +1,5 @@
 const Web3 = require("web3");
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8544"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 // const web3 = new Web3(new Web3.providers.IpcProvider(config.ipc_file, net));
 
 const config = require("../config.js")(web3, "liveNet");
@@ -11,4 +11,4 @@ const account = "0x54d9249C776C56520A62faeCB87A00E105E8c9Dc";
 
 // Setup contract objects
 const CS_contract = web3.eth.contract(cs_abi);
-const crowdsale = CS_contract.new(config.MW_address, config.startTime, config.endTime, account, config.tranches, config.milieurs_per_eth, {from: account, data: "0x" + cs_bytecode, gasPrice: 10000000000, gas: 5000000});
+const crowdsale = CS_contract.new(config.MW_address, config.startTime, config.endTime, account, config.tranches, config.milieurs_per_eth, {from: account, data: "0x" + cs_bytecode, gasPrice: 5000000000, gas: 5000000});
