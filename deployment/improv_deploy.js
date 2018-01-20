45fcc7928b05ae45787e14ee9db8b21aeaa82719
mguttman <martin.guttman@coinfabrik.com>
2018-01-19 22:02:45 -0300

    fix

--- deployment/improv_deploy.js
+++ deployment/improv_deploy.js
@@ -1,5 +1,5 @@
 const Web3 = require("web3");
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8544"));
 // const web3 = new Web3(new Web3.providers.IpcProvider(config.ipc_file, net));
 
 const config = require("../config.js")(web3, "liveNet");
