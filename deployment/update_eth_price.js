d8f607f990541d827ad98be6d2eab904d57b00ea
Horacio <horacio.gomez@coinfabrik.com>
2018-01-19 21:44:25 -0300

    modified eth price calculation

--- deployment/update_eth_price.js
+++ deployment/update_eth_price.js
@@ -9,6 +9,6 @@ const account = "0x54d9249C776C56520A62faeCB87A00E105E8c9Dc";
 const CS_contract = web3.eth.contract(cs_abi);
 CS_instance = CS_contract.at("0xb9aC67A866b2e6990538ad5C9c20b830E5404518")
 
-new_rate = new BigNumber(900).times(1000);
+new_rate = 900 * 1000;
 
 CS_instance.updateEursPerEth(new_rate, {from: account, gasPrice: 10000000000, gas: 200000});
