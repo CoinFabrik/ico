879fb742da66991ab922991dafb5b8cfa633ff66
Horacio <horacio.gomez@coinfabrik.com>
2018-02-23 13:52:06 -0300

    Cleanup binaries.

--- deployment/crowdsale.py
+++ deployment/crowdsale.py
@@ -23,39 +23,43 @@ with open("build/CrowdsaleToken.abi") as token_abi_file:
 token_contract = eth.contract(token_abi)
 
 def trans(from_index=0, value=0):
-  return {"from": eth.accounts[from_index], "value": value*(10**18), "gas": 4700000, "gasPrice": 50000000000}
+  return {"from": eth.accounts[from_index], "value": value*(10**18), "gas": 4700000, "gasPrice": 2000000000}
 
 #tokens_per_wei = [30*(10**25), 35*(10**25), 40*(10**25), 45*(10**25), 50*(10**25), 55*(10**25), 60*(10**25)]
 #amounts = [10000000000000000000, 9090909090909090909, 8333333333333333333, 7142857142857142857, 5882352941176470588, 5000000000000000000, 4000000000000000000]
 
-tokens_per_wei = [2]
+tokens_per_wei = [500]
 amounts = [525 * (10 ** 5) * (10 **18)]
 
 assert len(tokens_per_wei) == len(amounts)
 
 one_year = 60*60*24*365
-start = int(time.time()) + 60*2000
+start = int(time.time()) + 60*10
 end = start + one_year
 
 start_times = [start] * len(tokens_per_wei)
 end_times = [end] * len(tokens_per_wei)
 
-mw_address = "0xCAF3CAF3CAF3CAF3CAF3CAF3CAF3CAF3CAF3CAF3"
+mw_address = eth.accounts[1]
+
+assert len(mw_address) == 42;
+assert mw_address[0:2] == "0x"
+
 token_retriever = eth.accounts[1]
 tranches = list(itertools.chain.from_iterable(zip(amounts, start_times, end_times, tokens_per_wei)))
 print(tranches)
 params = [mw_address, start, end, token_retriever, tranches]
 
 
-#dHash = contract.deploy(transaction=trans(1,0), args=params)
+dHash = contract.deploy(transaction=trans(1,0), args=params)
 
 def status(tx_receipt):
   time.sleep(2)
   return eth.getTransactionReceipt(tx_receipt)["status"]
 
 def addAddress():
-  #receipt = eth.getTransactionReceipt(dHash)
-  contract.address = "0x896e6F7aC97eded5c927123ef8Efdd8255727D26"
+  receipt = eth.getTransactionReceipt(dHash) 
+  #contract.address = 
   print("Crowdsale:", contract.address)
   token_contract.address = token()
   print("Token:", token_contract.address)
@@ -85,5 +89,5 @@ def balances():
   for i in range(len(eth.accounts)):
     print(balance(i))
 
-time.sleep(2)
-addAddress()
+#time.sleep(2)
+#addAddress()
--- unlock.js
+++ unlock.js
@@ -1,4 +1,7 @@
 for (var j = 0; j < eth.accounts.length; j++) {
-	personal.unlockAccount(eth.accounts[j], "cuentahoracio0", 22000);
 	console.log(web3.fromWei(eth.getBalance(eth.accounts[j]), "ether"));
+}
+
+for (var j = 0; j < eth.accounts.length; j++) {
+	personal.unlockAccount(eth.accounts[j], "cuentahoracio0", 22000);
 }
