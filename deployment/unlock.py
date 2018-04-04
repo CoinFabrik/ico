40202b955d3261137e955832e4ae955e8b2ee234
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-04 14:40:19 -0300

    Unlock class made and added web3 base interface

--- deployment/unlock.py
+++ deployment/unlock.py
@@ -1,6 +1,12 @@
-web3 = None
+from web3_interface import Web3Interface
 
-def unlock():
-	for x in web3.eth.accounts:
-		web3.personal.unlockAccount(x, 'cuentahoracio0', 1800)
-
+class Unlock:
+  
+  web3 = None
+  
+  def __init__(self):
+    self.web3 = Web3Interface("middleware").w3
+  
+  def unlock(self):
+    for x in self.web3.eth.accounts:
+      self.web3.personal.unlockAccount(x, 'cuentahoracio0', 1800)
