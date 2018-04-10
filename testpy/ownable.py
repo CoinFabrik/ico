7aebb00e8028d85213b303202e7a3fc32455a6a8
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-10 12:01:27 -0300

    Updated ownable unit test

--- testpy/ownable.py
+++ testpy/ownable.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
-from deployer import Deployer
-from web3_interface import Web3Interface
-from tx_checker import fails, succeeds
+from util.deployer import Deployer
+from util.web3_interface import Web3Interface
+from util.tx_checker import fails, succeeds
 
 web3 = Web3Interface(middleware=True).w3
 
@@ -10,10 +10,10 @@ deployer = Deployer()
 
 owner = web3.eth.accounts[0]
 new_owner = web3.eth.accounts[1]
-gas = 50000000
+gas = 5000000
 gas_price = 20000000000
 
-(ownable, receipt) = deployer.deploy("./OwnerBuild/", "Ownable", owner, {"from": owner, "value": 0, "gas": gas, "gasPrice": gas_price},)
+(ownable, receipt) = deployer.deploy("./build/ownable/", "Ownable", owner, {"from": owner, "value": 0, "gas": gas, "gasPrice": gas_price},)
 
 assert ownable.functions.owner().call() == owner
 
