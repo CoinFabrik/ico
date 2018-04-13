323bf1a0dabb43e35b568721fc86aee2e368c20f
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-13 16:11:48 -0300

    Made getCurrentPrice public

--- contracts/TokenTranchePricing.sol
+++ contracts/TokenTranchePricing.sol
@@ -93,7 +93,7 @@ contract TokenTranchePricing {
   /// @dev Get the current price. May revert if there is no tranche currently active.
   /// @param tokensSold total amount of tokens sold, for calculating the current tranche
   /// @return The current price
-  function getCurrentPrice(uint tokensSold) internal view returns (uint result) {
+  function getCurrentPrice(uint tokensSold) public view returns (uint result) {
     return getCurrentTranche(tokensSold).price;
   }
 
