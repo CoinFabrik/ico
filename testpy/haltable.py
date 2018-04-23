577365405fdc63002b411bb0444dc8d5aa2659de
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-23 16:44:02 -0300

    Updated unit tests

--- testpy/haltable.py
+++ testpy/haltable.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
 
-import sys
-from deployer import deployer
-from checker import fails, succeeds
+from util.deployer import Deployer
+from util.web3_interface import Web3Interface
+from util.tx_checker import fails, succeeds
 
-http_provider = sys.argv[1]
+web3 = Web3Interface().w3
+web3.miner.start(1)
+sender = web3.eth.accounts[0]
+tx = {'from': sender, 'gas': 100000000, 'gasPrice': 20000000000}
+deployer = Deployer()
 
-web3 = Web3(HTTPProvider(http_provider))
+(haltable_contract, tx_hash) = deployer.deploy("./build/haltable/", "Haltable", sender, tx,)
+web3.eth.waitForTransactionReceipt(tx_hash)
+assert haltable_contract.functions.halted().call() == False
+succeeds("Halt succeeds", haltable_contract.functions.halt().transact(tx))
+assert haltable_contract.functions.halted().call() == True
+succeeds("Unhalt succeeds", haltable_contract.functions.unhalt().transact(tx))
+assert haltable_contract.functions.halted().call() == False
 
-tx = {'from': account,
-  'to': self.contract.address,
-  'value': value,
-  'gas': 100000000
-  }
-
-deployer = Deployer(web3, )
-
-haltable = deployer.deploy("Haltable")
-
-assert haltable.functions.halted().call() == False
-
-succeeds(haltable.functions.halt().transact(tx))
-
-assert haltable.functions.halted().call() == True
-
-succeeds(haltable.unhalt().call())
-
-assert haltable.functions.halted().call() == False
+web3.miner.stop()
