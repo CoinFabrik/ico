c2d5c20a0041bf95e003710c558373d53856be65
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-23 16:45:07 -0300

    Added compiling script for unit testing

---
+++ testpy/compile_test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir -p build
+cd build
+mkdir -p haltable
+mkdir -p ownable
+mkdir -p token_tranche_pricing
+cd ../../contracts
+solc -o ../testpy/build/haltable --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol
+solc -o ../testpy/build/ownable --abi --bin --overwrite --optimize --optimize-runs 0 Ownable.sol
+solc -o ../testpy/build/token_tranche_pricing --abi --bin --overwrite --optimize --optimize-runs 0 TokenTranchePricingMock.sol
+cd ../testpy
