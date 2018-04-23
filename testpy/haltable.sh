3e7a9df154fa801265d1d307e787b8aa69f00668
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-23 16:38:55 -0300

    Compiling for unit test unified into compile_test.sh

--- testpy/haltable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-cd build
-mkdir -p haltable
-cd ../../contracts
-solc -o ../testpy/build/haltable --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol
-cd ../testpy
