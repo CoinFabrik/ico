2195f377d333d5cd392f8a51ae76d719f33870d7
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-24 16:09:51 -0300

    Added utilities

---
+++ testpy/util/unlock.py
@@ -0,0 +1,11 @@
+from .web3_interface import Web3Interface
+
+class Unlocker:
+  web3 = None
+      
+  def __init__(self):
+    self.web3 = Web3Interface().w3
+      
+  def unlock(self):
+    for x in self.web3.eth.accounts:
+      self.web3.personal.unlockAccount(x, 'cuentahoracio0', 90000)
