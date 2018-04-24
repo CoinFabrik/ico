bcb71caf187c033db4326208b1ff4023d11992cd
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-24 16:10:37 -0300

    Added mock contract

---
+++ contracts/UpgradeableTokenMock.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.4.18;
+
+import './UpgradeableToken.sol';
+import './StandardToken.sol';
+
+// Mock class for testing of UpgradeableToken
+contract UpgradeableTokenMock is UpgradeableToken, StandardToken {
+    
+  bool public canUp;
+
+  //constructor
+  function UpgradeableTokenMock(uint value) public
+  UpgradeableToken(msg.sender) {
+    mintInternal(msg.sender, value);
+    setCanUp(true);
+  }
+
+  function setCanUp(bool value) public {
+    canUp = value;
+  }
+
+  //Blocked to avoid change of tokens amount except from upgrading
+  function transfer(address, uint) public returns (bool) {
+    return true;
+  }
+
+
+  /**
+   * Overriden for testing different values
+   */
+  function canUpgrade() public view returns(bool) {
+     return canUp;
+  }
+}
