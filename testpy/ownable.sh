2506436999515dae2541c25407c06825d4b07e94
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-24 13:04:39 -0300

    Now all compilation scripts in compile_test.sh

--- testpy/ownable.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-cd build
-mkdir -p ownable
-cd ../../contracts
-solc -o ../testpy/build/ownable --abi --bin --overwrite --optimize --optimize-runs 0 Ownable.sol
-cd ../testpy
--- testpy/token_tranche_pricing.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-cd build
-mkdir -p token_tranche_pricing
-cd ../../contracts
-solc -o ../testpy/build/token_tranche_pricing --abi --bin --overwrite --optimize --optimize-runs 0 TokenTranchePricingMock.sol
-cd ../testpy
