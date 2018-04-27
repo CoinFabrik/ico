6b0454dca13655fd2f93994e17575c264b18bd62
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-27 10:34:47 -0300

    Added StandardTokenMock to contracts dir

---
+++ contracts/StandardTokenMock.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.18;
+
+import './StandardToken.sol';
+
+// Mock class for StandardToken contract
+contract StandardTokenMock is StandardToken {
+  function mint(address receiver, uint amount) public {
+    mintInternal(receiver, amount);
+  }
+  
+  /**
+   * Made public for availability in tests
+   */
+  function burnTokensMock(address account, uint value) public {
+    super.burnTokens(account, value);
+  }
+}
