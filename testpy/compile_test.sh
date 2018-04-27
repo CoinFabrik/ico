7ac4945e2e3d784959e8bbf4b2fb011d631112fd
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-04-27 16:01:30 -0300

    Added MintableTokenMock to compilation

--- testpy/compile_test.sh
+++ testpy/compile_test.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 cd ../contracts
-solc -o ../testpy/build --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol Ownable.sol TokenTranchePricingMock.sol UpgradeableTokenMock.sol UpgradeAgentMock.sol StandardToken.sol
+solc -o ../testpy/build --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol Ownable.sol TokenTranchePricingMock.sol UpgradeableTokenMock.sol UpgradeAgentMock.sol StandardTokenMock.sol MintableTokenMock.sol
 cd ../testpy
