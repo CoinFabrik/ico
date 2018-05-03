80b43cc413766b4c7af89ca79cf7cac43967d160
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-03 14:52:52 -0300

    Updated utility

--- testpy/util/deployer.py
+++ testpy/util/deployer.py
@@ -1,7 +1,6 @@
 from .web3_interface import Web3Interface
 import json
 from .address import generate_contract_address
-from time import sleep
 from .load_contract import ContractLoader
 
 class Deployer:
@@ -10,10 +9,10 @@ class Deployer:
   def __init__(self):
     self.web3 = Web3Interface().w3
 
-  def deploy(self, path, contract_name, sender_account, tx_args, *args):
+  def deploy(self, path, contract_name, tx_args, *args):
     (contract_abi, contract_bytecode) = ContractLoader.get_abi_and_bytecode(path, contract_name)
-    deployer_nonce = self.web3.eth.getTransactionCount(sender_account)
-    contract_address = generate_contract_address(sender_account, deployer_nonce)
+    deployer_nonce = self.web3.eth.getTransactionCount(tx_args["from"])
+    contract_address = generate_contract_address(tx_args["from"], deployer_nonce)
     contract = self.web3.eth.contract(address=contract_address, abi=contract_abi, bytecode=contract_bytecode)
     tx_hash = contract.constructor(*args).transact(transaction=tx_args)
     return (contract, tx_hash)
