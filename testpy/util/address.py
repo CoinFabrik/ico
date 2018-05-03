da0c7d365936b2133ba5e063550bf821edbe47af
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-03 14:55:14 -0300

    Removed util directory

--- testpy/util/address.py
+++
@@ -1,5 +0,0 @@
-import rlp
-from eth_utils import keccak, to_checksum_address
-
-def generate_contract_address(address, nonce):
-  return to_checksum_address('0x' + keccak(rlp.encode([bytes(bytearray.fromhex(address[2:])), nonce]))[-20:].hex())
--- testpy/util/unlocker.py
+++
@@ -1,6 +0,0 @@
-from .web3_interface import Web3Interface
-
-def unlock():
-  web3 = Web3Interface().w3
-  for x in web3.eth.accounts:
-    web3.personal.unlockAccount(x, 'cuentahoracio0', 10000)
