aa6577e7199223d0153ac2284408086694faaf78
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-03 14:47:50 -0300

    Updated compilation script

--- testpy/compile_test.sh
+++ testpy/compile_test.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 cd ../contracts
-solc -o ../testpy/build --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol Ownable.sol TokenTranchePricingMock.sol UpgradeableTokenMock.sol UpgradeAgentMock.sol StandardTokenMock.sol MintableTokenMock.sol
+solc -o ../testpy/build --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol OwnableMock.sol TokenTranchePricingMock.sol UpgradeableTokenMock.sol UpgradeAgentMock.sol StandardTokenMock.sol MintableTokenMock.sol LostAndFoundTokenMock.sol GenericCrowdsaleMock.sol
 cd ../testpy
