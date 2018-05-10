093bb9f97acf8dc1a14ac87bcda12c32078e2b6f
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-10 12:38:11 -0300

    Updated compilation script

--- testpy/compile_test.sh
+++ testpy/compile_test.sh
@@ -1,13 +1,6 @@
 #!/bin/sh
 
 mkdir -p build
-cd build
-mkdir -p haltable
-mkdir -p ownable
-mkdir -p token_tranche_pricing
-mkdir -p upgradeable_token
-cd ../../contracts
-solc -o ../testpy/build/haltable --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol
-solc -o ../testpy/build/ownable --abi --bin --overwrite --optimize --optimize-runs 0 Ownable.sol
-solc -o ../testpy/build/token_tranche_pricing --abi --bin --overwrite --optimize --optimize-runs 0 TokenTranchePricingMock.sol
+cd ../contracts
+solc -o ../testpy/build/ --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol OwnableMock.sol TokenTranchePricingMock.sol UpgradeableTokenMock.sol UpgradeAgentMock.sol StandardTokenMock.sol MintableTokenMock.sol LostAndFoundTokenMock.sol GenericCrowdsaleMock.sol ReleasableToken.sol
 cd ../testpy
