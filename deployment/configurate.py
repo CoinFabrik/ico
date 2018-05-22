73a0acd83be78783fdb177864e6b7ca83445537a
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-22 15:20:42 -0300

    Updated main scripts

--- deployment/configurate.py
+++ deployment/configurate.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
-from datetime import datetime
-import json
-import time
-import os, errno
-import glob
-import unlocker
-from web3_interface import Web3Interface
-from load_contract import ContractLoader
-import sys
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--network", default="poanet", help="Enter network, defaults to poanet")
-parser.add_argument("-p", "--provider", default="http", help="Enter provider, defaults to http")
-parser.add_argument("-a", "--address", help="Enter address to look for log file")
-parser.add_argument("-d", "--deployment_name", help="Enter deployment name to look for log file")
-parser.add_argument("-t", "--test", action="store_true", help="Testing mode")
-args = parser.parse_args()
-
+from web3_interface import Web3Interface, args
+from contract import Contract
+from tx_dump import dump
+from tx_args import tx_args
 if args.test:
   from test_config import config_f
 else:
@@ -29,72 +13,14 @@ config = config_f()
 c = [config['MW_address'], config['start_time'], config['end_time'], config['token_retriever_account'], config['tranches'], config['multisig_supply'], config['crowdsale_supply'], config['token_decimals'], config['max_tokens_to_sell']]
 
 web3 = Web3Interface().w3
-miner = web3.miner
-if web3.net.chainId == "1":
+if web3.net.version == "1":
   sender_account = "0x54d9249C776C56520A62faeCB87A00E105E8c9Dc"
 else:
   sender_account = web3.eth.accounts[0]
-gas = 5000000
-gas_price = None
-log_path = "./log/"
 
-loader = ContractLoader()
-contract = loader.load("./build/", "Crowdsale", log_path=log_path)
-
-# Display configuration parameters, confirm them, write them to json file
-def dump():
-  # Displaying configuration parameters ----------------------------------------------------------------------------------
-  print("\nWeb3 version:", web3.version.api)
-  print("\nWeb3 network:", args.network)
-  print(
-    "\n\nMultisig address:", config['MW_address'][0],
-    "\n\nStart time:", time.ctime(config['start_time']),
-    "\n\nEnd time:", time.ctime(config['end_time']),
-    "\n\nToken retriever: " + config['token_retriever_account']
-  );  
-  for x in range(0,int((len(config['tranches'])/4)-1)):
-    print("\nTranche #", x, " -----------------------------------------------------------------",
-      "\nFullTokens cap:", int(config['tranches'][4*x]/(10**18)),
-      "\nStart:         ", time.ctime(config['tranches'][4*x+1]),
-      "\nEnd:           ", time.ctime(config['tranches'][4*x+2]),
-      "\nTokens per EUR:", config['tranches'][4*x+3]
-    )  
-  print("------------------------------------------------------------------------------");
-  print("\nTransaction sender: ", sender_account,
-        "\nGas and Gas price: ", gas, " and ", gas_price, "\n"
-  )  
-  # ----------------------------------------------------------------------------------------------------------------------
-  # Validating configuration parameters
-  pending_input = True
-  consent = None
-  while pending_input:
-    consent = input('\nDo you agree with the information? [yes/no]: ')
-    if consent == 'yes':
-      pending_input = False
-    elif consent == 'no':
-      sys.exit("Aborted")
-    else:
-      print("\nPlease enter 'yes' or 'no'\n")
-  
-  ContractLoader.exists_folder(log_path)
-  
-  # Writing configuration parameters into json file for logging purposes
-  (log_json, file_path) = ContractLoader.get_deployment_json_and_path(log_path, deployment_name=args.deployment_name, address=args.address)
-  log_json.update(config)
-  with open(file_path, 'w') as fp:
-    json.dump(log_json, fp, sort_keys=True, indent=2)
-
-def configurate():
-  configuration_tx_hash = contract.functions.configurationCrowdsale(*c).transact({"from": sender_account, "value": 0, "gas": gas, "gasPrice": gas_price})
-  print("Configuration transaction hash: ", configuration_tx_hash.hex())
-  return configuration_tx_hash
-
-if __name__ == '__main__':
-  if args.test:
-    unlocker.unlock()
-    miner.start(1)
-    gas_price = 20000000000
-  else:
-    gas_price = input("Enter gas price: ")
-    dump()
-  configurate()
+contract = Contract()
+contract = contract.load_contract("./build/", "Crowdsale")
+tx_args_dict = tx_args(sender_account, gas=2000000)
+dump(web3, sender_account, args, config, tx_args_dict["gas"], tx_args_dict["gasPrice"])
+configuration_tx_hash = contract.functions.configurationCrowdsale(*c).transact(tx_args_dict)
+print("Configuration transaction hash: ", configuration_tx_hash.hex())
---
+++ deployment/deploy.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from web3_interface import Web3Interface, args
+import json
+from datetime import datetime
+from contract import Contract
+from log import write_address_to_log
+from tx_args import tx_args
+
+web3 = Web3Interface().w3
+if web3.net.version == "1":
+  sender_account = "0x54d9249C776C56520A62faeCB87A00E105E8c9Dc"
+else:
+  sender_account = web3.eth.accounts[0]
+compiled_path = "./build/"
+contract = Contract()
+print("\nDeploying contract...")
+(crowdsale_contract, tx_hash) = contract.deploy(compiled_path, "Crowdsale", tx_args(sender_account, gas=4500000),)
+print("\nDeployment transaction hash: ", tx_hash.hex(),
+      "\nCrowdsale address: ", crowdsale_contract.address)
+write_address_to_log(crowdsale_contract)
