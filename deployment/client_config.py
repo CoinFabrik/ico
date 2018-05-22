38a318a45b8d839ec49888cc79b5e1ac22e74aa3
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-22 15:22:13 -0300

    Updated client config

--- deployment/client_config.py
+++ deployment/client_config.py
@@ -12,7 +12,6 @@ def config_f():
   config['crowdsale_supply'] = 525 * (10 ** 5) * (10 ** 18)
   config['multisig_supply'] = 525 * (10 ** 5) * (10 ** 18)
 
-  #Main net configuration
   tokens_per_wei = [410, 390, 370, 350, 340, 330, 320, 310, 300, 290, 280]
   tranches_quantity = len(tokens_per_wei)
   amounts = [3500000, 7000000, 10500000]
@@ -25,26 +24,26 @@ def config_f():
 
   amounts = list(map(toWei, amounts))
 
-  config['start_time'] = int(datetime(2018, 4, 10).timestamp())
-  config['end_time'] = int(datetime(2018, 7, 7).timestamp())
+  config['start_time'] = int(datetime(2018, 6, 1).timestamp())
+  config['end_time'] = int(datetime(2018, 8, 17).timestamp())
   config['MW_address'] = to_checksum_address("0xA8c39c22822a89bC8EAC413a1FFb93b73fb9c906")
   config['token_retriever_account'] = to_checksum_address("0x0F048ff7dE76B83fDC14912246AC4da5FA755cFE")
 
-  tranches_start = int(datetime(2018, 4, 6, 18).timestamp())
+  tranches_start = int(datetime.now().timestamp())
 
-  tranches_end = [int(datetime(2018, 4, 17).timestamp()),
-                  int(datetime(2018, 4, 24).timestamp()),
-                  int(datetime(2018, 5, 5).timestamp()),
-                  int(datetime(2018, 5, 12).timestamp()),
-                  int(datetime(2018, 5, 19).timestamp()),
-                  int(datetime(2018, 5, 26).timestamp()),
-                  int(datetime(2018, 6, 2).timestamp()),
-                  int(datetime(2018, 6, 9).timestamp()),
-                  int(datetime(2018, 6, 20).timestamp()),
-                  int(datetime(2018, 7, 2).timestamp()),
+  tranches_end = [int(datetime(2018, 6, 8).timestamp()),
+                  int(datetime(2018, 6, 15).timestamp()),
+                  int(datetime(2018, 6, 22).timestamp()),
+                  int(datetime(2018, 6, 29).timestamp()),
+                  int(datetime(2018, 7, 6).timestamp()),
+                  int(datetime(2018, 7, 13).timestamp()),
+                  int(datetime(2018, 7, 20).timestamp()),
+                  int(datetime(2018, 7, 27).timestamp()),
+                  int(datetime(2018, 8, 3).timestamp()),
+                  int(datetime(2018, 8, 10).timestamp()),
                   config['end_time']]
 
-  for x in range(0, tranches_quantity):
+  for x in range(tranches_quantity):
     config['tranches'].append(amounts[x])
     config['tranches'].append(tranches_start)
     config['tranches'].append(tranches_end[x])
