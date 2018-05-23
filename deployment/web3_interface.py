4128cc6fd105d2fa2148765537b3aa8fc5662b34
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-05-23 15:14:42 -0300

    Updated constructor

--- deployment/web3_interface.py
+++ deployment/web3_interface.py
@@ -9,18 +9,16 @@ class Web3Interface:
     provider_param = None
     with open("../deployment/networks.json") as networks_file:
       networks = json.load(networks_file)
-    try:
-      if args.provider == "ipc":
-        provider_param = networks[args.network][args.provider]["path"]
-        self.w3 = Web3(Web3.IPCProvider(provider_param))
-      elif args.provider == "ws":
-        provider_param = args.provider + "://" + networks[args.network][args.provider]["host"] + ":" + str(networks[args.network][args.provider]["port"])
-        self.w3 = Web3(Web3.WebsocketProvider(provider_param))
-      else:
-        provider_param = args.provider + "://" + networks[args.network][args.provider]["host"] + ":" + str(networks[args.network][args.provider]["port"])
-        self.w3 = Web3(Web3.HTTPProvider(provider_param))
-    except IndexError:
-      print("IndexError")
-      pass
+    if args.provider == "ipc":
+      provider_param = networks[args.network][args.provider]["path"]
+      self.w3 = Web3(Web3.IPCProvider(provider_param))
+    elif args.provider == "ws":
+      provider_param = args.provider + "://" + networks[args.network][args.provider]["host"] + ":" + str(networks[args.network][args.provider]["port"])
+      self.w3 = Web3(Web3.WebsocketProvider(provider_param))
+    elif args.provider == "http":
+      provider_param = args.provider + "://" + networks[args.network][args.provider]["host"] + ":" + str(networks[args.network][args.provider]["port"])
+      self.w3 = Web3(Web3.HTTPProvider(provider_param))
+    else:
+      raise ValueError("Provider not within the admitted options.")
     if (args.network == "poanet"):
       self.w3.middleware_stack.inject(geth_poa_middleware, layer=0)
