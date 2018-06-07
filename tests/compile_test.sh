d343b1fbabc650e8049b3587a64a8ecb98db9310
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-06-07 17:02:28 -0300

    Now compiling Ownable instead of OwnableMock

--- tests/compile_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-mkdir -p build
-cd ../contracts
-solc -o ../testpy/build/ --abi --bin --overwrite --optimize --optimize-runs 0 Haltable.sol OwnableMock.sol TokenTranchePricingMock.sol UpgradeableTokenMock.sol UpgradeAgentMock.sol StandardTokenMock.sol MintableTokenMock.sol LostAndFoundTokenMock.sol GenericCrowdsaleMock.sol ReleasableToken.sol
-cd ../testpy
