1c1feefad077a1b836b076f5993aed049fe253b5
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-06-14 14:49:43 -0300

    Added granularPreallocate function for contract migration

--- contracts/GenericCrowdsale.sol
+++ contracts/GenericCrowdsale.sol
@@ -184,6 +184,25 @@ contract GenericCrowdsale is Haltable {
   }
 
   /**
+   * Preallocate tokens for the early investors.
+   *
+   * Preallocated tokens have been sold before the actual crowdsale opens.
+   * This function mints the tokens and moves the crowdsale needle.
+   *
+   * No money is exchanged, as the crowdsale team already have received the payment.
+   *
+   * @param receiver Account that receives the tokens.
+   * @param tokenAmount Indivisible tokens.
+   * @param weiAmount Equivalent of tokens in wei.
+   *
+   */
+  function granularPreallocate(address receiver, uint tokenAmount, uint weiAmount) public onlyOwner notFinished {
+    require(receiver != address(0));
+    require(tokenAmount != 0);
+    updateInvestorFunds(tokenAmount, weiAmount, receiver , 0);
+  }
+
+  /**
    * Private function to update accounting in the crowdsale.
    */
   function updateInvestorFunds(uint tokenAmount, uint weiAmount, address receiver, uint128 customerId) private {
