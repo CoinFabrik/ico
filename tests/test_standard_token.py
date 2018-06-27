6c92bd9a76dabb7c73a9ef6d66a98781f410ccfa
mathiasestolarz <mathias.e.stolarz@gmail.com>
2018-06-27 16:22:31 -0300

    Added unit test

---
+++ tests/test_standard_token.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import pytest
+import sys
+sys.path.append("../deployment")
+from contract import Contract
+from tx_args import tx_args
+from web3_interface import Web3Interface
+
